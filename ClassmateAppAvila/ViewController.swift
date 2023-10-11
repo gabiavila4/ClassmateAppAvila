3b7830d75d1299fcc4ee757622ba325bc6bf8e6c
Gabriela Avila <gabrielanicoleavila@icloud.com>
2023-10-11 09:16:09 -0500

    Initial Commit

---
+++ ClassmateAppAvila/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassmateAppAvila
+//
+//  Created by GABRIELA AVILA on 10/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
